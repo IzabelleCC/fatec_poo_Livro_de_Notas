3b67dd8ce0992832def568fb41c97489228d7a87
Izabelle Cesario <izabellecesario@hotmail.com>
2023-08-31 18:04:48 -0300

    ap06(java): teste livro de notas

---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,8 @@
+public class TesteLivroDeNotas{
+    public static void main(String args []){
+        //variável de referencia
+        LivroDeNotas livroDeNotas = new LivroDeNotas();
+        livroDeNotas.exibirMensagem();
+        System.out.println("Até Logooo!!");
+    }
+}
